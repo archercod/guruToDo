b68f25667730713ade51a33a6b1619445ab00b06
Marcin Pietrzak <pietrzakmarcin@hotmail.com>
2019-07-04 21:16:22 +0200

    Initial commit

---
+++ GuruToDo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GuruToDo
+//
+//  Created by Marcin Pietrzak on 04/07/2019.
+//  Copyright © 2019 Marcin Pietrzak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
